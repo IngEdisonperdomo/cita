6c82608fef7c004bf1306269c239f0fe7defe951
Edison <edison@ucrop.it>
2021-05-24 16:48:34 -0400

    add background header

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 const App = () => {
   return (
-    <>
-      <Text>Citas</Text>
-    </>
+    <View style={styles.contenedor}>
+      <Text style={styles.titulo}>Administrador de citas</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#aa076b',
+    flex: 1,
+  },
+  titulo: {
+    color: '#fff',
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default App;
